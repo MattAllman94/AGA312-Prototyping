64b7ac7a273d82fb469be9fd57f5b8b4eed94524
MattAllman94 <62230366+MattAllman94@users.noreply.github.com>
2021-03-10 19:05:31 +1100

    Week 4

--- Assets/Universal/Scripts/Scoring.cs
+++ Assets/Universal/Scripts/Scoring.cs
@@ -1,26 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+//Using TMP;
 
 public class Scoring : JMC
 {
 
-    public int lastRoundScore = 100;
-    public int thisRoundScore = 150;
-    public int lives = 5;
+    public int highScore = 0;
+    public int score = 150;
 
-    void Start()
+    [Header("UI Elements")]
+    public CanvasGroup scoreCanvas;
+    public Text scoreText;
+    public Text highScoreText;
+    public Text scoreMessage;
+
+    private void Start()
     {
-        if(IsGameOver(lives))
+        FadeCanvas(scoreCanvas, 0, 0);
+        GetHighScore();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
         {
             CheckScore();
         }
-
+        //PlayerPrefs.DeleteKey("HighScore")
     }
 
     void CheckScore()
     {
-        print("Score difference is : " + PercentageCheck(lastRoundScore, thisRoundScore).ToString("F1") + "%");
+        GetHighScore();
+        highScoreText.text = highScore.ToString();
+        scoreText.text = score.ToString();
+
+        if (score > highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            scoreMessage.text = "Congratulations! \n You got a new High Score!";
+            //TMP <color = green> - changes the color of the text
+            scoreMessage.gameObject.transform.DOScale(Vector3.one * 1.2f, 2).SetLoops(-1);
+
+        }
+        else
+        {
+            scoreMessage.text = "Too Bad! \n Better luck next time!";
+            scoreMessage.gameObject.transform.DOScale(Vector3.one * 0.8f, 2).SetLoops(-1);
+        }
+
+        FadeCanvas(scoreCanvas, 1, 1);
+
+
+    }
+
+    void GetHighScore()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore");
+        Debug.Log("The high score is " + highScore);
     }
 
 }
